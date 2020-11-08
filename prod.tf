2be52fe3881202bd26314f2dc33d1b7aab17703a
fred aklamanu <fred.aklamanu@nokia-bell-labs.com>
2020-11-08 07:50:10 +0100

    Refactored to use asg

--- prod.tf
+++ prod.tf
@@ -80,7 +80,7 @@ resource "aws_eip" "prod_web" {
 
 resource "aws_elb" "prod_web" {
     name            = "prod-web"
-    instances       = aws_instance.prod_web.*.id
+#    instances       = aws_instance.prod_web.*.id
     subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
     security_groups = [aws_security_group.prod_web.id]
 
@@ -90,5 +90,35 @@ resource "aws_elb" "prod_web" {
         lb_port           = 80
         lb_protocol       = "http"
     }
+}
+
+resource "aws_launch_template" "prod_web" {
+name_prefix   = "prod_web"
+image_id      = "ami-055fc45692cb976ff"
+instance_type = "t2.micro"
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+availability_zones  = null
+vpc_zone_identifier = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+desired_capacity    = 1
+max_size            = 1
+min_size            = 1
+
+launch_template {
+    id      = aws_launch_template.prod_web.id
+    version = "$Latest"
+    }
+    tag {
+        key               = "Terraform"  
+        value             = "true"
+        propagate_at_launch = "true"
+    }
+}
+
+
+resource "aws_autoscaling_attachment" "prod_web" {
+  autoscaling_group_name = aws_autoscaling_group.prod_web.id
+  elb                    = aws_elb.prod_web.id
 }
 
